50402bb80818d34c707f5ef78ea710115b19945b
ThomasTJTJ <thomasj2023@gmail.com>
2026-04-01 13:28:12 -0500

    Add files via upload
    Very rough but A frame work

---
+++ project2aWithOpt4.ino
@@ -0,0 +1,697 @@
+#include "lcdhelperv2.h"
+#include "irhelper.h"
+#include <ESP32Servo.h>
+#include "httphelper.h"
+#include <DabbleESP32.h>
+//
+//  Create an instance of the lcdhelper class named oLCD
+//
+lcdhelper oLCD(ILI9163_4L, 3, 2, 9, 10, 7);
+//
+//  Create an instance of irhelper class named oIR.
+//
+irhelper oIR;
+//
+//  Define function prototypes.
+//
+// led pins defined for option 1
+const int LED1 = 19;
+const int LED2 = 20;
+const int LED3 = 21;
+const int LED4 = 22;
+
+const int SW1_PIN = 18;
+const int SW2_PIN = 17;
+const int SW3_PIN = 16;
+const int SW4_PIN = 15;
+
+const int SPEAKER_PIN = 8;
+
+const int MAX_CS_PIN = 10;
+const int MAX_CLK_PIN = 12;
+const int MAX_SO_PIN = 13;
+// MOSFET PIN
+const int PWM_PIN = 11;
+// optocoupler
+const int OPTO_PIN = 38;
+// tilt and pan
+const int TILT_SERVO_PIN = 35;
+const int PAN_SERVO_PIN = 36;
+
+// DAN- add lines 35-40 to finish main document steps 13-15
+// Speaker channel
+const int SPK_CHANNEL = 1;
+// PWM channel
+const int PWM_CHANNEL = 2;
+// Audio channel
+const int AUDIO_CHANNEL = 4;
+
+void ShowDisplay(screen val, char optionstate, char keypressed, bool intdisplay);
+
+void Option1();
+void Option2(char optionstate);
+void Option3(char optionstate, char keypressed);
+void Option4(char keypressed);
+void Option5();
+
+Servo pservo;
+Servo tservo;
+
+int pangle = 90;             // angle to 90 degrees
+int tangle = 90;             // angle to 90 degrees
+bool servosAttached = false; // setting servos as unattached
+
+// FOR OPTION 4
+//  Thread Stuff
+TaskHandle_t Task_PlayAudio;
+
+// HTTP helper
+httphelper objHTTP;
+
+// File arrays
+String picFiles[16];
+String audioFiles[16];
+
+// Index
+int currentIndex = 0;
+
+// FOR OPTION 4
+
+//
+//  Setup()
+//
+void setup()
+{
+    Serial.begin(115200);
+    Dabble.begin("Group3");
+    while (!Serial)
+    {
+        // wait for serial to start
+    }
+    // led pins
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+    pinMode(LED4, OUTPUT);
+
+    // switch pin inputs
+    pinMode(SW1_PIN, INPUT_PULLUP);
+    pinMode(SW2_PIN, INPUT_PULLUP);
+    pinMode(SW3_PIN, INPUT_PULLUP);
+    pinMode(SW4_PIN, INPUT_PULLUP);
+
+    // speaker pin
+    pinMode(SPEAKER_PIN, OUTPUT);
+
+    // MAX6675 pins
+    pinMode(MAX_CS_PIN, OUTPUT);
+    pinMode(MAX_CLK_PIN, OUTPUT);
+    pinMode(MAX_SO_PIN, INPUT);
+    digitalWrite(MAX_CS_PIN, HIGH);
+
+    ESP32PWM::allocateTimer(3); // set servo objects to 20 milliseconds
+    // MOSFET Pin
+    pinMode(PWM_PIN, OUTPUT);
+
+    // optocouple pin
+    pinMode(OPTO_PIN, INPUT);
+
+    // tilt and pan servo pins
+    pservo.setPeriodHertz(50);
+    tservo.setPeriodHertz(50);
+    pinMode(TILT_SERVO_PIN, OUTPUT);
+    pinMode(PAN_SERVO_PIN, OUTPUT);
+    tservo.attach(TILT_SERVO_PIN);
+    pservo.attach(PAN_SERVO_PIN);
+    tservo.write(90);
+    pservo.write(90);
+    delay(500);
+    tservo.detach();
+    pservo.detach();
+
+    // call to setup & attachpin correction
+    ledcSetup(SPK_CHANNEL, 496, 8);
+    ledcAttachPin(SPEAKER_PIN, SPK_CHANNEL);
+
+    // Initialize IR Communications
+    oIR.IRInitialize();
+    // Show main menu when Arduino starts
+    // for the 1st time or on reset.
+    ShowDisplay(SC_MAIN, ' ', ' ', true);
+
+    // FOR OPTION 4
+    xTaskCreate(PlayAudio_loop, "Task_PlayAudio", 1000, NULL, 1, &Task_PlayAudio);
+
+    GetFileInfo(); // MUST be before menu
+    // FOR OPTION 4
+}
+//
+//  Loops(): Perform looping operations
+//
+void loop()
+{
+    unsigned long last_key_processed;
+    // Determine which key was pressed.
+    last_key_processed = oIR.GetKeyPressed();
+    // Return to the main menu any time the
+    // menu key is pressed.
+    if (last_key_processed == KEY_MENU)
+    {
+        ShowDisplay(SC_MAIN, ' ', ' ', false);
+    }
+    else
+    {
+        if (oLCD.SCREEN_STATE == SC_MAIN)
+        {
+            if (last_key_processed == KEY_1)
+            {
+                ShowDisplay(SC_SUB1, ' ', ' ', false);
+            }
+            else if (last_key_processed == KEY_2)
+            {
+                HandleGamepad();
+                ShowDisplay(SC_SUB2, 'A', ' ', false);
+            }
+            else if (last_key_processed == KEY_3)
+            {
+                ShowDisplay(SC_SUB3, 'A', ' ', false);
+            }
+            else if (last_key_processed == KEY_4)
+            {
+                ShowDisplay(SC_SUB4, ' ', ' ', false);
+            }
+            else if (last_key_processed == KEY_5)
+            {
+                ShowDisplay(SC_SUB5, ' ', ' ', false);
+            }
+            else if (last_key_processed == KEY_RETURN)
+            {
+                ShowDisplay(SC_MAIN, ' ', ' ', false);
+            }
+        }
+        else if (oLCD.SCREEN_STATE == SC_SUB1) // WARNINGS
+        {
+            if (last_key_processed == KEY_RETURN)
+            {
+                ShowDisplay(SC_MAIN, ' ', ' ', false);
+            }
+        }
+        else if (oLCD.SCREEN_STATE == SC_SUB2)
+        {
+            if (last_key_processed == KEY_RETURN)
+            {
+                tservo.detach(); // detaching servos when leaving option2
+                pservo.detach();
+                servosAttached = false; // setting servos to detached
+                ShowDisplay(SC_MAIN, ' ', ' ', false);
+                ShowDisplay(SC_MAIN, ' ', ' ', false);
+            }
+        }
+        else if (oLCD.SCREEN_STATE == SC_SUB3) // WARNINGS
+        {
+            if (last_key_processed == KEY_RETURN)
+            {
+                ShowDisplay(SC_MAIN, ' ', ' ', false);
+            }
+        }
+        else if (oLCD.SCREEN_STATE == SC_SUB4)
+        {
+            if (last_key_processed == KEY_RETURN)
+            {
+                ShowDisplay(SC_MAIN, ' ', ' ', false);
+            }
+        }
+        else if (oLCD.SCREEN_STATE == SC_SUB5)
+        {
+            if (last_key_processed == KEY_RETURN)
+            {
+                ShowDisplay(SC_MAIN, ' ', ' ', false);
+            }
+        }
+    }
+    delay(100);
+}
+// *************************************************************************************
+// BEGIN: FUNCTION DEFINITIONS
+// *************************************************************************************
+//
+//  Method to display menus on TFT display
+//
+void ShowDisplay(screen val, char optionstate, char keypressed, bool initdisplay)
+{
+    // Store screen state.
+    oLCD.SCREEN_STATE = val;
+    oLCD.OPTION_STATE = optionstate;
+    int selectedpid = 0;
+    char text[20];
+
+    if (val == SC_MAIN)
+    {
+        oLCD.LCDInitialize(LANDSCAPE, initdisplay);
+        sprintf(text, "SELECT OPTION");
+        oLCD.print(text, 10, 10);
+        sprintf(text, "1. Mystery Music");
+        oLCD.print(text, 10, 25);
+        sprintf(text, "2. Pan and Tilt");
+        oLCD.print(text, 10, 40);
+        sprintf(text, "3. Run the Motor");
+        oLCD.print(text, 10, 55);
+        sprintf(text, "4. Stream Audio");
+        oLCD.print(text, 10, 70);
+        sprintf(text, "5. Query Sensors");
+        oLCD.print(text, 10, 85);
+    }
+    else if (val == SC_SUB1)
+    {
+        Option1();
+    }
+    else if (val == SC_SUB2)
+    {
+        Option2(optionstate);
+    }
+    else if (val == SC_SUB3)
+    {
+        Option3(optionstate, keypressed);
+    }
+    else if (val == SC_SUB4)
+    {
+        Option4(keypressed);
+    }
+    else if (val == SC_SUB5)
+    {
+        Option5();
+    }
+}
+//********************************************************************
+//  BEGIN OPTION CODE
+//********************************************************************
+void Option1()
+{
+    oLCD.LCDInitialize(LANDSCAPE, false);
+    // set background color
+    oLCD.fillscreen(RCB_PURPLE);
+    // print music playing
+    oLCD.print("MUSIC PLAYING", 10, 10);
+
+    if (Playing())
+    {
+        playSong();
+    }
+    else
+    {
+    }
+}
+
+bool playSong()
+{
+    // song contains the sounds
+    // note contains the length
+    uint16_t song[1] = {13}; // temp array because the actual array is over 1400 elements
+    uint16_t note[1] = {14}; // temp array because the actual array is over 1400 elements
+
+    int N = sizeof(song) / sizeof(song[0]); // # elements in array song is 1439 and notes is 1414, use size of operato
+
+    for (int i = 0; i < N; i++)
+    {
+        if ((i % 8 == 6) || (i % 8 == 7))
+        {
+            continue;
+        }
+        int duration = note[i];
+
+        ledcWriteTone(SPK_CHANNEL, song[i]);
+
+        if (digitalRead(SW1_PIN) == LOW)
+        {
+            duration = duration / 2;
+        }
+
+        if (digitalRead(SW1_PIN) == HIGH)
+        {
+            duration = duration * 2;
+        }
+
+        int ledIndex = i % 4;
+
+        digitalWrite(LED1, LOW);
+        digitalWrite(LED2, LOW);
+        digitalWrite(LED3, LOW);
+        digitalWrite(LED4, LOW);
+
+        if (ledIndex == 0)
+            digitalWrite(LED1, HIGH);
+        if (ledIndex == 1)
+            digitalWrite(LED2, HIGH);
+        if (ledIndex == 2)
+            digitalWrite(LED3, HIGH);
+        if (ledIndex == 3)
+            digitalWrite(LED4, HIGH);
+
+        delay(duration);
+
+        // ledcWriteTone(SPK_CHANNEL, song[i]);
+
+        // delay(duration);
+
+        ledcWriteTone(SPK_CHANNEL, 0);
+
+        digitalWrite(LED1, LOW);
+        digitalWrite(LED2, LOW);
+        digitalWrite(LED3, LOW);
+        digitalWrite(LED4, LOW);
+
+        if (digitalRead(SW4_PIN) == LOW)
+        {
+            ledcWriteTone(SPK_CHANNEL, 0);
+
+            oLCD.clrScr();
+            oLCD.print("MSC STOPPED", 10, 10);
+            oLCD.print("PRESS <MODE>", 10, 30);
+
+            return false;
+        }
+    }
+    oLCD.clrScr();
+    oLCD.print("MSC FINISHED", 10, 10);
+    oLCD.print("PRESS <RETURN>", 10, 30);
+
+    ledcWriteTone(SPK_CHANNEL, 0);
+
+    return true; // song finished
+}
+// End of option 1
+
+void AttachServos() // initialize a function to attach servos
+{
+    if (servosAttached == false) // seeing if servos are set to attached
+    {
+        tservo.attach(TILT_SERVO_PIN); // attaching tilt servo
+        pservo.attach(PAN_SERVO_PIN);  // attaching pan servo
+
+        tservo.write(tangle); // moving servo to specified angle
+        pservo.write(pangle); // moving servo to specified angle
+
+        servosAttached = true; // setting servos to attached
+    }
+}
+void ServoScreenUpdate()
+{
+    char text[20]; // create character array
+    sprintf(text, "PAN ANGLE");
+    oLCD.print(text, CENTER, 30);          // output text(PAN ANGLE) to screen
+    oLCD.printNumF(pangle, 4, CENTER, 45); // output pan angle number to screen
+
+    sprintf(text, "TILT ANGLE");
+    oLCD.print(text, CENTER, 60);          // output text(tilt ANGLE) to screen
+    oLCD.printNumF(tangle, 4, CENTER, 75); // output tilt angle number to screen
+
+    sprintf(text, "PRESS <RETURN>")
+        oLCD.print(text, CENTER, 90); // output "PRESS <RETURN>" to the screen
+
+    sprintf(text, "TO GO BACK")
+        oLCD.print(text, CENTER, 105); // output "TO GO BACK" to the screen
+}
+void HandleGamepad()
+{
+    Dabble.processInput(); // starts collecting data from dabble
+
+    if (GamePad.isUpPressed()) // Up button pressed
+    {
+        tangle += 5           // increase tilt angle by 5 degrees
+            if (tangle > 165) // see if tilt angle is going above maximum
+        {
+            tangle = 165; // set tilt angle at maximum
+        }
+        tservo.write(tangle); // write the tilt angle to the specified amount
+    }
+    if (Gamepad.isDownPressed()) // Down button is pressed
+    {
+        tangle -= 5          // decrease tilt angle by 5 degrees
+            if (tangle < 15) // see if tilt angle is going below minimum
+        {
+            tangle = 15; // set tilt angle at minimum
+        }
+        tservo.write(tangle);
+    }
+    if (Gamepad.isLeftPressed()) // Left button is pressed
+    {
+        pangle += 5;      // pan 5 degrees to the left
+        if (pangle > 165) // see if pan angle is at the max
+        {
+            pangle = 165; // set pan angle to max if going above
+        }
+        pservo.write(pangle); // move servo to specified angle
+    }
+    if (Gamepad.isRightPressed()) // Right button is pressed
+    {
+        pangle -= 5;     // pan 5 degrees to the right
+        if (pangle < 15) // see if the pan angle is at the min
+        {
+            pangle = 15; // set pan angle at min if going below
+        }
+        pservo.write(pangle); // move servo to specified angle
+    }
+    // update the display of new angles
+    ServoScreenUpdate() // calls function to update ther servo screen
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+}
+
+void Option2(char optionstate)
+{
+    ////////////////////////////////////////////////////////////////////////////////////
+    AttachServos(); // call function to attach servos
+    oLCD.LCDInitialize(LANDSCAPE, false);
+    //////////////////////////////////////////////////////////
+    char = text[20]; // create character array
+    sprintf(text, "PAN ANGLE");
+    oLCD.print(text, CENTER, 30);          // output text(PAN ANGLE) to screen
+    oLCD.printNumF(pangle, 4, CENTER, 45); // output pan angle number to screen
+
+    sprintf(text, "TILT ANGLE");
+    oLCD.print(text, CENTER, 60);          // output text(tilt ANGLE) to screen
+    oLCD.printNumF(tangle, 4, CENTER, 75); // output tilt angle number to screen
+
+    sprintf(text, "PRESS <RETURN>")
+        oLCD.print(text, CENTER, 90); // output "PRESS <RETURN>" to the screen
+
+    sprintf(text, "TO GO BACK")
+        oLCD.print(text, CENTER, 105); // output "TO GO BACK" to the screen
+    ////////////////////////////////////////////////////////////
+    if ()
+        pservo.attach(PAN_SERVO_PIN);
+    tservo.attach(TILT_SERVO_PIN);
+}
+//
+//  Code for Option3
+//
+void Option3(char optionstate, char keypressed)
+{
+    oLCD.LCDInitialize(LANDSCAPE, false);
+}
+//
+//  Option to Move between pictures and songs.
+//
+// FOR OPTION 4
+void GetFileInfo()
+{
+    oLCD.LCDInitialize(LANDSCAPE, false);
+    oLCD.print("CONNECTING", 10, 20);
+    oLCD.print("TO WIFI", 10, 40);
+    oLCD.print(".....", 10, 60);
+
+    HTTPClient m_http;
+
+    if (objHTTP.connect(30))
+    {
+        if (objHTTP.getURL(m_http,
+                           "https://intranet.mne.ksu.edu/static/ME400/images.php", 3000))
+        {
+
+            GetBaseFiles(m_http, picFiles, audioFiles, 16);
+
+            m_http.end();
+        }
+        objHTTP.disconnect();
+    }
+}
+
+void MysteryPic(String filename)
+{
+
+    HTTPClient m_http;
+    unsigned char img[2560];
+
+    if (!objHTTP.connect(30))
+        return;
+
+    String url = "https://intranet.mne.ksu.edu/static/ME400/" + filename;
+
+    if (objHTTP.getURL(m_http, url, 3000))
+    {
+
+        String data = m_http.getString();
+
+        int idx = data.indexOf("128");
+        idx = data.indexOf('\n', idx);
+
+        if (cleanData.length() < 2560)
+            return;
+
+        for (int i = 0; i < 2560; i++)
+        {
+
+            unsigned char c = (unsigned char)cleanData[i];
+
+            unsigned char reversed = 0;
+
+            for (int b = 0; b < 8; b++)
+            {
+                if (bitRead(c, b))
+                {
+                    bitWrite(reversed, 7 - b, 1);
+                }
+            }
+
+            img[i] = reversed;
+        }
+
+        oLCD.drawXBitmap(0, 0, img, 160, 128, RCB_WHITE);
+
+        m_http.end();
+    }
+
+    objHTTP.disconnect();
+}
+void MysteryAudio(String filename)
+{
+
+    HTTPClient m_http;
+
+    ledcDetachPin(audioPin);
+
+    sigmaDeltaSetup(audioPin, audioChannel, 2000000);
+    sigmaDeltaWrite(audioChannel, 0);
+
+    if (!objHTTP.connect(30))
+        return;
+
+    String url = "https://intranet.mne.ksu.edu/static/ME400/" + filename;
+
+    if (objHTTP.getURL(m_http, url, 3000))
+    {
+
+        circle_init();
+
+        WiFiClient *stream = m_http.getStreamPtr();
+        int len = m_http.getSize();
+
+        while (m_http.connected() && (len > 0 || len == -1))
+        {
+
+            size_t size = stream->available();
+
+            if (size)
+            {
+
+                uint8_t buff[200];
+                int bytes_to_insert = min(sizeof(buff), size);
+
+                int c = stream->readBytes(buff, bytes_to_insert);
+
+                if (len > 0)
+                    len -= c;
+
+                // REQUIRED LOOP (MOST IMPORTANT PART)
+                for (int i = 0; i < c; i++)
+                {
+
+                    while (!circle_put(buff[i]))
+                    {
+                        delay(10);
+                    }
+                }
+            }
+
+            // Stop if user presses button
+            if (digitalRead(SW1_PIN) == LOW ||
+                digitalRead(SW2_PIN) == LOW ||
+                digitalRead(SW3_PIN) == LOW ||
+                digitalRead(SW4_PIN) == LOW)
+            {
+                break;
+            }
+        }
+
+        sigmaDeltaWrite(audioChannel, 0);
+
+        m_http.end();
+        objHTTP.disconnect();
+    }
+
+    void PlayAudio_loop(void *parameter)
+    {
+        while (true)
+        {
+            if (circle_available())
+            {
+                uint8_t val = circle_get();
+                sigmaDeltaWrite(audioChannel, val);
+            }
+        }
+    }
+}
+void Option4(char keypressed)
+{
+    oLCD.LCDInitialize(LANDSCAPE, false);
+    currentIndex = 0;
+
+    MysteryPic(picFiles[currentIndex]);
+    MysteryAudio(audioFiles[currentIndex]);
+
+    while (true)
+    {
+
+        int button = oIR.GetKeyPressed();
+
+        // Forward >>
+        if (button == IR_FWD)
+        {
+            currentIndex++;
+            if (currentIndex >= 16)
+                currentIndex = 0;
+
+            MysteryPic(picFiles[currentIndex]);
+            MysteryAudio(audioFiles[currentIndex]);
+        }
+
+        // Reverse <<
+        if (button == IR_REV)
+        {
+            currentIndex--;
+            if (currentIndex < 0)
+                currentIndex = 15;
+
+            MysteryPic(picFiles[currentIndex]);
+            MysteryAudio(audioFiles[currentIndex]);
+        }
+
+        // Exit
+        if (button == IR_MENU)
+        {
+            return;
+        }
+        delay(50);
+    }
+}
+
+//
+//  Thermocouple implementation Entry Point
+//
+void Option5()
+{
+    oLCD.LCDInitialize(LANDSCAPE, false);
+}
+//********************************************************************
+//  END OPTION CODE
+//********************************************************************
